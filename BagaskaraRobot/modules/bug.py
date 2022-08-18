97e7d66ccf2b6fee6fdc5b01d8aaa20e27daa0d6
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2022-08-18 12:23:21 +0700

    bug

--- BagaskaraRobot/modules/bug.py
+++ BagaskaraRobot/modules/bug.py
@@ -46,10 +46,10 @@ async def bug(_, msg: Message):
     datetimes_fmt = "%d-%m-%Y"
     datetimes = datetime.utcnow().strftime(datetimes_fmt)
 
-    thumb = "https://telegra.ph/file/a589fe0ca2b883b7a3dc3.jpg"
+    thumb = "https://telegra.ph/file/d8530063bb68cca946cd8.jpg"
     
     bug_report = f"""
-**#BUG : ** **[Master](https://t.me/ybgskr)**
+**#BUG : ** **[Master](https://t.me/Rey_ab14)**
 **From User : ** **{mention}**
 **User ID : ** **{user_id}**
 **Group : ** **{chat_username}**
